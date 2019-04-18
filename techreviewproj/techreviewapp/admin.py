b5c0f90cdac7aee479c6e8521694d0c4377f8f44
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-04-18 12:10:46 -0700

    4th Commit - creates Model Class for db tables

--- techreviewproj/techreviewapp/admin.py
+++ techreviewproj/techreviewapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, TechType ,Review
 
 # Register your models here.
+admin.site.register(TechType)
+admin.site.register(Product)
+admin.site.register(Review)
--- techreviewproj/techreviewapp/models.py
+++ techreviewproj/techreviewapp/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TechType(models.Model):
+    techtypename=models.CharField(max_length=255)
+    techdescription=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.techtypename
+
+    class Meta:
+        db_table='techtype'
+        verbose_name_plural='techtypes'    
+
+class Product(models.Model):
+    productname=models.CharField(max_length=255)
+    producttype=models.ForeignKey(TechType, on_delete=models.DO_NOTHING)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    productprice=models.DecimalField(max_digits=10, decimal_places=2)
+    productentrydate=models.DateField()
+    producturl=models.URLField(null=True, blank=True)
+    productdescription=models.TextField(null=True, blank=True)
+
+    #returns the amount of discount for members
+    def memberDiscount(self):
+        discount=.05
+        return float(self.productprice) * discount
+
+    #returns the discounted price for members
+    def discountedPrice(self):
+        discount=self.memberDiscount()
+        return float(self.productprice) - discount    
+
+    def __str__(self):
+        return self.productname
+
+    class Meta:
+        db_table='product'
+        verbose_name_plural='products' 
+
+class Review(models.Model):
+    reviewtitle=models.CharField(max_length=255)
+    reviewdate=models.DateField()
+    product=models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    user=models.ManyToManyField(User)
+    rating=models.SmallIntegerField()
+    reviewtext=models.TextField()
+
+
+    def __str__(self):
+        return self.reviewtitle
+
+    class Meta:
+        db_table='review'
+        verbose_name_plural='reviews'
