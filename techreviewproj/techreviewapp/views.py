0fa3c819fe7a095218d905ec3df377b0378f17c4
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-04-25 11:42:35 -0700

    5-creates view to display data

--- techreviewproj/techreviewapp/views.py
+++ techreviewproj/techreviewapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import TechType, Product, Review
 
 # Create your views here.
 def index (request):
     return render(request, 'techreviewapp/index.html')
+
+def gettypes(request):
+    types_list=TechType.objects.all()
+    context={'types_list' : types_list }
+    return render(request, 'techreviewapp/types.html', context=context)    
