741e26cf9385bf31e97ac09019c25bf6ed06f8e9
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-04-30 12:07:47 -0700

    6 view and details

--- techreviewproj/techreviewapp/urls.py
+++ techreviewproj/techreviewapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
+    path('getproducts/', views.getproducts, name='products'),
+    path('productdetail/<int:id>', views.productdetails, name='productdetail'),
 ]
--- techreviewproj/techreviewapp/views.py
+++ techreviewproj/techreviewapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TechType, Product, Review
 
 # Create your views here.
@@ -9,3 +9,18 @@ def gettypes(request):
     types_list=TechType.objects.all()
     context={'types_list' : types_list }
     return render(request, 'techreviewapp/types.html', context=context)    
+
+def getproducts(request):
+    product_list=Product.objects.all()
+    return render(request, 'techreviewapp/products.html', {'product_list': product_list})
+
+def productdetails(request, id):
+    prod=get_object_or_404(Product, pk=id)   
+    reviews=Review.objects.filter(product=id)
+    reviewcount=Review.objects.filter(product=id).count()
+    context={
+        'prod' : prod,
+        'reviewcount' : reviewcount,
+        'reviews' : reviews,
+    }
+    return render(request, 'techreviewapp/productdetails.html', context=context)
